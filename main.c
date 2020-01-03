3bc52f2f926ff451de638504d5a52e8f99da5869
Michelle Giraldo <michellegsld@gmail.com>
2020-01-02 20:13:44 -0500

    2nd Pushing, Including way to read each line from the file, Contains two ways by using getline

--- main.c
+++ main.c
@@ -9,7 +9,11 @@
  */
 int main(int argc, char *argv[])
 {
-    FILE *fin = fopen(argv[1],"r");
+	FILE *fin = fopen(argv[1],"r");
+	char *str_buf = NULL;
+    size_t line_count = 0;
+    size_t str_buf_len = 0;
+    //ssize_t str_len = 0;
 
 	if (argc != 2)
 	{
@@ -17,15 +21,30 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-    if (fin == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	if (fin == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	//str_len = getline(&str_buf, &str_buf_len, fin);
+
+    //while (str_len >= 0)
+    //{
+    //    line_count++;
+    //   printf("Line number: %d, Line contents: %s\n", str_buf_len, str_buf);
+    //    str_len = getline(&str_buf, &str_buf_len, fin);
+    //}
+
+	while(getline(&str_buf, &str_buf_len, fin) != -1)
+	{
+        line_count++;
+		printf("This is the line: \"%s\".\n", str_buf);
+	}
 
 	printf("The argument count amount was 2 and the file \"%s\" could be opened.\n", argv[1]);
 
-    fclose(fin);
+	fclose(fin);
 
-    return 0;
+	return 0;
 }
