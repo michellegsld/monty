54fd65f55f10b9584898af7cda26ab2af00de40d
Michelle Giraldo <michellegsld@gmail.com>
2020-01-03 23:49:46 -0500

    2nd Pushing, Adding more opcodes, Also accounts for the opcode 'nop' case

--- get_opcode_func.c
+++ get_opcode_func.c
@@ -1,14 +1,19 @@
 #include "monty.h"
 
 /**
- * get_opcode_func - Checks if the string is a valid opcode and calls it if is
+ * *get_opcode_func - Checks if the string is a valid opcode and calls it if is
  * @op: The string to check if is an opcode
+ *
+ * Return: The opcode's function or NULL
  */
 void (*get_opcode_func(char *op))(stack_t **, unsigned int)
 {
 	instruction_t opcodes[] = {
 		{"push", op_push},
 		{"pall", op_pall},
+		{"pint", op_pint},
+		{"pop", op_pop},
+		{"swap", op_swap},
 		{"add", op_add},
 		{"sub", op_sub},
 		{"mul", op_mul},
@@ -18,7 +23,7 @@ void (*get_opcode_func(char *op))(stack_t **, unsigned int)
 	};
 	size_t i = 0;
 
-	while (opcodes[i].opcode != NULL)
+	while (opcodes[i].opcode != NULL && (strcmp(op, "nop") != 0))
 	{
 		if (strcmp(opcodes[i].opcode, op) == 0)
 			return (opcodes[i].f);
