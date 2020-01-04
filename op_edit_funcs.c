5fd9782647656ad3285477b5e55bf5455295b842
Michelle Giraldo <michellegsld@gmail.com>
2020-01-03 23:51:12 -0500

    2nd Pushing, Adding the last two functions: op_pop and op_swap

--- op_edit_funcs.c
+++ op_edit_funcs.c
@@ -7,8 +7,9 @@
  */
 void op_push(stack_t **head, unsigned int line_num)
 {
-	stack_t *new;
+	stack_t *new = malloc(sizeof(stack_t));
 	int num;
+	unsigned int i = 0;
 
 	if (push_tok != NULL)
 		num = atoi(push_tok);
@@ -19,7 +20,16 @@ void op_push(stack_t **head, unsigned int line_num)
 		exit(EXIT_FAILURE);
 	}
 
-	new = malloc(sizeof(stack_t));
+	while (i < strlen(push_tok))
+	{
+		if (!isdigit(push_tok[i]))
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_num);
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+
 	if (new == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
@@ -36,6 +46,48 @@ void op_push(stack_t **head, unsigned int line_num)
 	*head = new;
 }
 
-//Functions to add to this file:
-//	- pop : Removes the top element of the stack
-//	- swap : Swaps the top two elements of the stack
+/**
+ * op_pop - Removes the top element of the stack
+ * @head: The top of the stack
+ * @line_num: The current line number within the file
+ */
+void op_pop(stack_t **head, unsigned int line_num)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*head)->next)
+	{
+		*head = (*head)->next;
+		free((*head)->prev);
+		(*head)->prev = NULL;
+	}
+	else
+	{
+		free(*head);
+		*head = NULL;
+	}
+}
+
+/**
+ * op_swap - Swaps the top two elements of the stack
+ * @head: The top of the stack
+ * @line_num: The current line number within the file
+ */
+void op_swap(stack_t **head, unsigned int line_num)
+{
+	int tmp;
+
+	if (stack_len(*head) < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = (*head)->n;
+	(*head)->n = (*head)->next->n;
+	(*head)->next->n = tmp;
+}
